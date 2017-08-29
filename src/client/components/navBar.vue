0cfb706b56817f157f3ecfb5c587b4234e2d0949
stephenliu <stephenlyao@yeah.net>
2017-08-29 22:57:55 +0800

    navbar

--- src/client/components/navBar.vue
+++ src/client/components/navBar.vue
@@ -1,21 +1,57 @@
 <template>
   <div class="nav-bar">
     <div class="logo">
-      <img :src="logo" alt="muxi-resume-logo" />
+      <img src="~/assets/img/logo.png" alt="muxi-resume-logo" />
     </div>
     <div class="menu">
-      
+      <router-link to="/" exact>
+        木犀云简历
+      </router-link>
+      <router-link to="/edit">
+        在线制作
+      </router-link>
+      <router-link to="/lists">
+        模版商城
+      </router-link>
     </div>
-    <div class="auth"></div>
+    <div class="auth">
+      <span class="searchBox"></span>
+      <template v-if="$store.state.user.isAuthenticated">
+      </template>
+      <template v-else>
+        <span @click="showSignIn">登录</span>
+        <span @click="showSignUp">注册</span>
+        <sign-form v-show="show" @closeSignForm="closeSignForm" mold="mold"></sign-form>
+      </template>
+    </div>
   </div>
 </template>
 
 <script>
-import logo from '~/assets/img/logo.png'
+import signForm from '~/components/signForm.vue'
+
 export default {
+  name: 'nav-bar',
   data() {
     return {
-      logo
+      show: false,
+      mold: null
+    }
+  },
+  components: {
+    signForm
+  },
+  methods: {
+    showSignIn: function() {
+      this.show = true
+      this.mold = 'signIn'
+    },
+    showSignUp: function() {
+      this.show = true
+      this.mold = 'signUp'
+    },
+    closeSignForm: function() {
+      this.show = false
     }
   }
 }
@@ -40,4 +76,13 @@ export default {
     img
       width: 100%
       height: 100%
+  .menu
+    margin-left: 65px
+    a
+      margin: 0 4px
+    .nuxt-link-active
+      color: #FFFFFF
+  .auth
+    float: right
+
 </style>
